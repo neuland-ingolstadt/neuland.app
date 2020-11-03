5929b49110e85a2f01338b5d52c5d30f52005c68
Alexander Horn <alexhorn@users.noreply.github.com>
2020-11-03 18:03:41 +0100

    Refactor thi-api-request.js

--- rogue-thi-app/lib/thi-api-request.js
+++ rogue-thi-app/lib/thi-api-request.js
@@ -1,34 +1,44 @@
+/**
+ * Client to interact with hiplan.thi.de via a websockify proxy.
+ */
+
 import * as forge from 'node-forge/lib/index.all'
 
-export function thiApiRequest (params) {
-  // T-TeleSec GlobalRoot Class 2
-  // valid until 02.10.2033, 1:59:59 CEST
-  const thiCerts = [
-    `-----BEGIN CERTIFICATE-----
-    MIIDwzCCAqugAwIBAgIBATANBgkqhkiG9w0BAQsFADCBgjELMAkGA1UEBhMCREUx
-    KzApBgNVBAoMIlQtU3lzdGVtcyBFbnRlcnByaXNlIFNlcnZpY2VzIEdtYkgxHzAd
-    BgNVBAsMFlQtU3lzdGVtcyBUcnVzdCBDZW50ZXIxJTAjBgNVBAMMHFQtVGVsZVNl
-    YyBHbG9iYWxSb290IENsYXNzIDIwHhcNMDgxMDAxMTA0MDE0WhcNMzMxMDAxMjM1
-    OTU5WjCBgjELMAkGA1UEBhMCREUxKzApBgNVBAoMIlQtU3lzdGVtcyBFbnRlcnBy
-    aXNlIFNlcnZpY2VzIEdtYkgxHzAdBgNVBAsMFlQtU3lzdGVtcyBUcnVzdCBDZW50
-    ZXIxJTAjBgNVBAMMHFQtVGVsZVNlYyBHbG9iYWxSb290IENsYXNzIDIwggEiMA0G
-    CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCqX9obX+hzkeXaXPSi5kfl82hVYAUd
-    AqSzm1nzHoqvNK38DcLZSBnuaY/JIPwhqgcZ7bBcrGXHX+0CfHt8LRvWurmAwhiC
-    FoT6ZrAIxlQjgeTNuUk/9k9uN0goOA/FvudocP05l03Sx5iRUKrERLMjfTlH6VJi
-    1hKTXrcxlkIF+3anHqP1wvzpesVsqXFP6st4vGCvx9702cu+fjOlbpSD8DT6Iavq
-    jnKgP6TeMFvvhk1qlVtDRKgQFRzlAVfFmPHmBiiRqiDFt1MmUUOyCxGVWOHAD3bZ
-    wI18gfNycJ5v/hqO2V81xrJvNHy+SE/iWjnX2J14np+GPgNeGYtEotXHAgMBAAGj
-    QjBAMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgEGMB0GA1UdDgQWBBS/
-    WSA2AHmgoCJrjNXyYdK4LMuCSjANBgkqhkiG9w0BAQsFAAOCAQEAMQOiYQsfdOhy
-    NsZt+U2e+iKo4YFWz827n+qrkRk4r6p8FU3ztqONpfSO9kSpp+ghla0+AGIWiPAC
-    uvxhI+YzmzB6azZie60EI4RYZeLbK4rnJVM3YlNfvNoBYimipidx5joifsFvHZVw
-    IEoHNN/q/xWA5brXethbdXwFeilHfkCoMRN3zUA7tFFHei4R40cR3p1m0IvVVGb6
-    g1XqfMIpiRvpb7PO4gWEyS8+eIVibslfwXhjdFjASBgMmTnrpMwatXlajRWc2BQN
-    9noHV8cigwUtPJslJj0Ys6lDfMjIq2SPDqO/nBudMNva0Bkuqjzx+zOAduTNrRlP
-    BSeOE6Fuwg==
-    -----END CERTIFICATE-----`
-  ]
+const ENDPOINT_HOST = 'hiplan.thi.de'
+const ENDPOINT_URL = '/webservice/production2/index.php'
+const USER_AGENT = 'Better THI-App https://github.com/M4GNV5/THI-App'
 
+const PROXY_URL = 'ws://localhost:8080'
+
+// T-TeleSec GlobalRoot Class 2
+// valid until 02.10.2033, 1:59:59 CEST
+const THI_CERTS = [
+  `-----BEGIN CERTIFICATE-----
+  MIIDwzCCAqugAwIBAgIBATANBgkqhkiG9w0BAQsFADCBgjELMAkGA1UEBhMCREUx
+  KzApBgNVBAoMIlQtU3lzdGVtcyBFbnRlcnByaXNlIFNlcnZpY2VzIEdtYkgxHzAd
+  BgNVBAsMFlQtU3lzdGVtcyBUcnVzdCBDZW50ZXIxJTAjBgNVBAMMHFQtVGVsZVNl
+  YyBHbG9iYWxSb290IENsYXNzIDIwHhcNMDgxMDAxMTA0MDE0WhcNMzMxMDAxMjM1
+  OTU5WjCBgjELMAkGA1UEBhMCREUxKzApBgNVBAoMIlQtU3lzdGVtcyBFbnRlcnBy
+  aXNlIFNlcnZpY2VzIEdtYkgxHzAdBgNVBAsMFlQtU3lzdGVtcyBUcnVzdCBDZW50
+  ZXIxJTAjBgNVBAMMHFQtVGVsZVNlYyBHbG9iYWxSb290IENsYXNzIDIwggEiMA0G
+  CSqGSIb3DQEBAQUAA4IBDwAwggEKAoIBAQCqX9obX+hzkeXaXPSi5kfl82hVYAUd
+  AqSzm1nzHoqvNK38DcLZSBnuaY/JIPwhqgcZ7bBcrGXHX+0CfHt8LRvWurmAwhiC
+  FoT6ZrAIxlQjgeTNuUk/9k9uN0goOA/FvudocP05l03Sx5iRUKrERLMjfTlH6VJi
+  1hKTXrcxlkIF+3anHqP1wvzpesVsqXFP6st4vGCvx9702cu+fjOlbpSD8DT6Iavq
+  jnKgP6TeMFvvhk1qlVtDRKgQFRzlAVfFmPHmBiiRqiDFt1MmUUOyCxGVWOHAD3bZ
+  wI18gfNycJ5v/hqO2V81xrJvNHy+SE/iWjnX2J14np+GPgNeGYtEotXHAgMBAAGj
+  QjBAMA8GA1UdEwEB/wQFMAMBAf8wDgYDVR0PAQH/BAQDAgEGMB0GA1UdDgQWBBS/
+  WSA2AHmgoCJrjNXyYdK4LMuCSjANBgkqhkiG9w0BAQsFAAOCAQEAMQOiYQsfdOhy
+  NsZt+U2e+iKo4YFWz827n+qrkRk4r6p8FU3ztqONpfSO9kSpp+ghla0+AGIWiPAC
+  uvxhI+YzmzB6azZie60EI4RYZeLbK4rnJVM3YlNfvNoBYimipidx5joifsFvHZVw
+  IEoHNN/q/xWA5brXethbdXwFeilHfkCoMRN3zUA7tFFHei4R40cR3p1m0IvVVGb6
+  g1XqfMIpiRvpb7PO4gWEyS8+eIVibslfwXhjdFjASBgMmTnrpMwatXlajRWc2BQN
+  9noHV8cigwUtPJslJj0Ys6lDfMjIq2SPDqO/nBudMNva0Bkuqjzx+zOAduTNrRlP
+  BSeOE6Fuwg==
+  -----END CERTIFICATE-----`
+]
+
+export function thiApiRequest (params) {
   function ab2str (buf) {
     return String.fromCharCode.apply(null, new Uint8Array(buf))
   }
@@ -47,12 +57,12 @@ export function thiApiRequest (params) {
 
   const options = {
     method: 'POST',
-    path: '/webservice/production2/index.php',
+    path: ENDPOINT_URL,
     body: paramList.join('&'),
     headers: {
-      Host: 'hiplan.thi.de',
+      Host: ENDPOINT_HOST,
       'Content-Type': 'application/x-www-form-urlencoded',
-      'User-Agent': 'Better THI-App https://github.com/M4GNV5/THI-App'
+      'User-Agent': USER_AGENT
     }
   }
 
@@ -60,70 +70,68 @@ export function thiApiRequest (params) {
   const buffer = forge.util.createBuffer()
   const response = forge.http.createResponse()
 
-  let resolveResult = null
-  let rejectResult = null
-  const resultPromise = new Promise(function (resolve, reject) {
-    resolveResult = resolve
-    rejectResult = reject
-  })
+  return new Promise((resolve, reject) => {
+    const client = forge.tls.createConnection({
+      server: false,
+      caStore: THI_CERTS,
+      virtualHost: ENDPOINT_HOST,
+      verify: function (connection, verified, depth, certs) {
+        if (certs[0].subject.getField('CN').value === ENDPOINT_HOST) {
+          return verified
+        } else {
+          return false
+        }
+      },
+      connected: function (connection) {
+        client.prepare(request.toString() + request.body)
+      },
+      tlsDataReady: function (connection) {
+        const data = connection.tlsData.getBytes()
+        socket.send(str2ab(data))
+      },
+      dataReady: function (connection) {
+        // clear data from the server is ready
+        const data = connection.data.getBytes()
+        if (response.bodyReceived) {
+          return
+        }
 
-  const client = forge.tls.createConnection({
-    server: false,
-    caStore: thiCerts,
-    virtualHost: 'hiplan.thi.de',
-    verify: function (connection, verified, depth, certs) {
-      if (certs[0].subject.getField('CN').value !== 'hiplan.thi.de') { return false }
-      return verified
-    },
-    connected: function (connection) {
-      client.prepare(request.toString() + request.body)
-    },
-    tlsDataReady: function (connection) {
-      const data = connection.tlsData.getBytes()
-      socket.send(str2ab(data))
-    },
-    dataReady: function (connection) {
-      // clear data from the server is ready
-      const data = connection.data.getBytes()
-      if (response.bodyReceived) { return }
+        buffer.putBytes(data)
+        if (!response.headerReceived) {
+          response.readHeader(buffer)
+        }
 
-      buffer.putBytes(data)
-      if (!response.headerReceived) {
-        response.readHeader(buffer)
-      }
-
-      if (response.headerReceived) {
-        if (response.readBody(buffer)) {
-          try {
-            const data = JSON.parse(response.body)
-            resolveResult(data)
-          } catch (e) {
-            rejectResult(e)
-          } finally {
-            client.close()
+        if (response.headerReceived) {
+          if (response.readBody(buffer)) {
+            try {
+              const data = JSON.parse(response.body)
+              resolve(data)
+            } catch (e) {
+              reject(e)
+            } finally {
+              client.close()
+            }
           }
         }
+      },
+      closed: function () {
+        socket.close()
+      },
+      error: function (connection, error) {
+        reject(error)
       }
-    },
-    closed: function () {
-      socket.close()
-    },
-    error: function (connection, error) {
-      rejectResult(error)
-    }
-  })
+    })
 
-  const socket = new WebSocket('ws://localhost:8080')
-  socket.binaryType = 'arraybuffer'
-  socket.addEventListener('open', function (event) {
-    client.handshake()
+    const socket = new WebSocket(PROXY_URL)
+    socket.binaryType = 'arraybuffer'
+    socket.addEventListener('open', function (event) {
+      client.handshake()
+    })
+    socket.addEventListener('message', function (event) {
+      client.process(ab2str(event.data))
+    })
+    socket.addEventListener('error', function (event) {
+      reject(event)
+    })
   })
-  socket.addEventListener('message', function (event) {
-    client.process(ab2str(event.data))
-  })
-  socket.addEventListener('error', function (event) {
-    rejectResult(event)
-  })
-
-  return resultPromise
 }
