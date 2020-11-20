5063685de422f5650ad976db890a7211a2846944
Jakob Löw <jakob@m4gnus.de>
2020-11-21 00:03:57 +0100

    improve the /debug page (fixes #4)

---
+++ rogue-thi-app/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#git pull
+#npm install
+npm run build
+systemctl restart thi-app.service
--- rogue-thi-app/pages/debug.js
+++ rogue-thi-app/pages/debug.js
@@ -1,32 +1,124 @@
-import React, { useState } from 'react'
-import styles from '../styles/Debug.module.css'
+import React, { useState, useEffect } from 'react'
+import { useRouter } from 'next/router'
+import styles from '../styles/Common.module.css'
+
+import { obtainSession } from '../lib/thi-api-client'
 import { thiApiRequest } from '../lib/thi-api-request'
 
+import Container from 'react-bootstrap/Container'
+import ListGroup from 'react-bootstrap/ListGroup'
+import Form from 'react-bootstrap/Form'
+import Button from 'react-bootstrap/Button'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+
 export default function Debug () {
-  const [json, setJson] = useState('')
+  const [parameters, setParameters] = useState([])
   const [result, setResult] = useState('')
+  const router = useRouter()
+
+  useEffect(async () => {
+    const initialParams = [
+      {
+        name: 'service',
+        value: 'thiapp',
+      },
+      {
+        name: 'method',
+        value: 'persdata',
+      },
+      {
+        name: 'session',
+        value: await obtainSession(router),
+      },
+      {
+        name: 'format',
+        value: 'json',
+      },
+    ]
+
+    setParameters(initialParams)
+  }, [])
+
+  function changeParameterName (i, name) {
+    parameters[i].name = name
+    setParameters(parameters.slice(0))
+  }
+  function changeParameterValue (i, value) {
+    parameters[i].value = value
+    setParameters(parameters.slice(0))
+  }
+  function addParameter () {
+    const newParams = parameters.concat([{
+      name: '',
+      value: '',
+    }])
+    setParameters(newParams)
+  }
+  function removeParameter (index) {
+    const newParams = parameters.slice(0)
+    newParams.splice(index, 1)
+    setParameters(newParams)
+  }
 
   async function submit () {
+    const params = {}
+    parameters.forEach(entry => params[entry.name] = entry.value)
     try {
-      const req = JSON.parse(json)
-      const resp = await thiApiRequest(req)
-      setResult(JSON.stringify(resp, null, 2))
-    } catch (e) {
+      setResult('Loading...')
+      const resp = await thiApiRequest(params)
+      setResult(JSON.stringify(resp, null, 4))
+    }
+    catch(e) {
       console.error(e)
+      setResult(e.toString())
     }
   }
 
   return (
-    <div className={styles.container}>
-        <textarea
-          className={styles.jsonText}
-          onChange={e => setJson(e.target.value)}
-          value={json}
-        />
-        <pre>
-          {result}
-        </pre>
-        <button onClick={submit}>Submit</button>
-    </div>
+    <Container>
+      <h1>Debug</h1>
+
+      <h3>Fields</h3>
+      <ListGroup>
+        {parameters && parameters.map((param, idx) =>
+          <ListGroup.Item key={idx} className={styles.item}>
+            <Row>
+              <Col>
+                <Form.Control
+                  type="text"
+                  placeholder="name"
+                  value={param.name}
+                  onChange={e => changeParameterName(idx, e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Form.Control
+                  type="text"
+                  placeholder="value"
+                  value={param.value}
+                  onChange={e => changeParameterValue(idx, e.target.value)}
+                />
+              </Col>
+              <Col>
+                <Button variant="danger" onClick={() => removeParameter(idx)}>
+                  Remove
+                </Button>
+              </Col>
+            </Row>
+          </ListGroup.Item>
+        )}
+      </ListGroup>
+      <br />
+      <Button variant="primary" onClick={submit}>Submit</Button>
+      {' '}
+      <Button variant="success" onClick={addParameter}>Add Field</Button>
+
+      <br />
+      <h3>Result</h3>
+      <pre>
+        {result}
+      </pre>
+    </Container>
   )
 }
