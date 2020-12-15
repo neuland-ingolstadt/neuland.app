285ea1314c98509277af09397afe62b5d24c7fb7
Alexander Horn <alexhorn@users.noreply.github.com>
2020-12-15 18:37:50 +0100

    Fix compile warning in AppNavbar.js

--- rogue-thi-app/lib/AppNavbar.js
+++ rogue-thi-app/lib/AppNavbar.js
@@ -26,8 +26,8 @@ export default function AppNavbar ({ title, showBack, children }) {
   return (
     <>
       <Head>
-        <meta charset='utf-8' />
-        <meta http-equiv='X-UA-Compatible' content='IE=edge' />
+        <meta charSet='utf-8' />
+        <meta httpEquiv='X-UA-Compatible' content='IE=edge' />
         <meta name='viewport' content='width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no' />
         <meta name='description' content='Rogue THI App' />
         <meta name='keywords' content='THI, Technische Hochschule Ingolstadt, App' />
