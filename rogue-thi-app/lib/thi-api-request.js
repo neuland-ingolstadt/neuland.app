b3dc2e2eb6f94cc248dc3e1ef6ecbbfa0067d1d5
Alexander Horn <alexhorn@users.noreply.github.com>
2020-12-15 18:37:28 +0100

    Move proxy URL to environment variable

--- rogue-thi-app/lib/thi-api-request.js
+++ rogue-thi-app/lib/thi-api-request.js
@@ -8,7 +8,7 @@ const ENDPOINT_HOST = 'hiplan.thi.de'
 const ENDPOINT_URL = '/webservice/production2/index.php'
 const USER_AGENT = 'Rogue THI-App https://github.com/M4GNV5/THI-App'
 
-const PROXY_URL = 'wss://proxy.neuland.app'
+const PROXY_URL = process.env.NEXT_PUBLIC_PROXY_URL
 
 // T-TeleSec GlobalRoot Class 2
 // valid until 02.10.2033, 1:59:59 CEST
