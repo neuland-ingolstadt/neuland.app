10ea22e3223b3b186cc5f25e98dc7e7f1f09935f
Alexander Horn <alexhorn@users.noreply.github.com>
2020-12-15 18:32:34 +0100

    Fix fontawesome icon size

--- rogue-thi-app/pages/_app.js
+++ rogue-thi-app/pages/_app.js
@@ -1,5 +1,12 @@
 import '../styles/globals.css'
+
+// bootstrap
 import 'bootstrap/dist/css/bootstrap.min.css'
+
+// fontawesome
+import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from '@fortawesome/fontawesome-svg-core'
+config.autoAddCss = false
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
