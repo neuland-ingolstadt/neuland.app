cf40d14ec12df996bfa9632b747d1f4eb91003af
Jakob Löw <jakob@m4gnus.de>
2021-04-03 00:18:43 +0200

    pre-initialize the matrix animation and improve it a bit

--- rogue-thi-app/components/TheMatrixAnimation.js
+++ rogue-thi-app/components/TheMatrixAnimation.js
@@ -3,65 +3,69 @@ import React, { useEffect, useState } from 'react'
 export default function TheMatrixAnimation () {
   const [canvasId, setCanvasId] = useState(null)
 
+  function matrixAnimationFrame (canvas, ctx, ypos) {
+    // Draw a semitransparent black rectangle on top of previous drawing
+    ctx.fillStyle = '#0001'
+    ctx.fillRect(0, 0, canvas.width, canvas.height)
+
+    // Set color to green and font to 15pt monospace in the drawing context
+    ctx.fillStyle = '#0f0'
+    ctx.font = '15pt monospace'
+
+    // for each column put a random character at the end
+    ypos.forEach((y, ind) => {
+      // generate a random character
+      const text = String.fromCharCode(Math.floor(Math.random() * (126 - 33) + 33))
+
+      // x coordinate of the column, y coordinate is already given
+      const x = ind * 20
+      // render the character at (x, y)
+      ctx.fillText(text, x, y)
+
+      if (y > 100 + Math.random() * 10000) {
+        // randomly reset the end of the column if it's at least 100px high
+        ypos[ind] = 0
+      }
+      else {
+        // otherwise just move the y coordinate for the column 20px down,
+        ypos[ind] = y + 20
+      }
+    })
+  }
+
   useEffect(() => {
     const id = Math.random().toString(36).substr(2, 8)
     setCanvasId(id)
 
     let canvas = null
     let ctx = null
-    let cols = null
     let ypos = null
     setInterval(() => {
       // the following code is mostly taken from
       // https://dev.to/gnsp/making-the-matrix-effect-in-javascript-din
 
-      if (!canvas || !ctx) {
+      if (!canvas) {
         canvas = document.getElementById(id)
-        console.log('found canvas', canvas, 'with id', id)
         if (!canvas) {
           return
         }
-        ctx = canvas.getContext('2d')
 
         canvas.width = window.innerWidth
         canvas.height = window.innerHeight
 
+        ctx = canvas.getContext('2d')
         ctx.fillStyle = '#000'
         ctx.fillRect(0, 0, canvas.width, canvas.height)
 
-        cols = Math.floor(canvas.width / 20) + 1
+        const cols = Math.floor(canvas.width / 20) + 1
         ypos = Array(cols).fill(0)
 
-        console.log(canvas, canvas.width, canvas.height)
+        for (let i = 0; i < 300; i++) {
+          matrixAnimationFrame(canvas, ctx, ypos)
+        }
       }
 
-      // Draw a semitransparent black rectangle on top of previous drawing
-      ctx.fillStyle = '#0001'
-      ctx.fillRect(0, 0, canvas.width, canvas.height)
-
-      // Set color to green and font to 15pt monospace in the drawing context
-      ctx.fillStyle = '#0f0'
-      ctx.font = '15pt monospace'
-
-      // for each column put a random character at the end
-      ypos.forEach((y, ind) => {
-        // generate a random character
-        const text = String.fromCharCode(Math.random() * 128)
-
-        // x coordinate of the column, y coordinate is already given
-        const x = ind * 20
-        // render the character at (x, y)
-        ctx.fillText(text, x, y)
-
-        if (y > 100 + Math.random() * 10000) {
-          // randomly reset the end of the column if it's at least 100px high
-          ypos[ind] = 0
-        }
-        else {
-          // otherwise just move the y coordinate for the column 20px down,
-          ypos[ind] = y + 20
-        }
-      })
+      matrixAnimationFrame(canvas, ctx, ypos)
     }, 50)
   }, [])
 
