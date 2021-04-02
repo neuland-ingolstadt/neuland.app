c7ba122ddabedf9da3df0a351e803116038170fe
Jakob Löw <jakob@m4gnus.de>
2021-04-03 00:50:59 +0200

    improve matrix initialization to make it less laggy in firefox

--- rogue-thi-app/components/TheMatrixAnimation.js
+++ rogue-thi-app/components/TheMatrixAnimation.js
@@ -3,6 +3,9 @@ import React, { useEffect, useState } from 'react'
 export default function TheMatrixAnimation () {
   const [canvasId, setCanvasId] = useState(null)
 
+  function getRandomChar () {
+    return String.fromCharCode(Math.floor(Math.random() * (126 - 33) + 33))
+  }
   function matrixAnimationFrame (canvas, ctx, ypos) {
     // Draw a semitransparent black rectangle on top of previous drawing
     ctx.fillStyle = '#0001'
@@ -14,19 +17,15 @@ export default function TheMatrixAnimation () {
 
     // for each column put a random character at the end
     ypos.forEach((y, ind) => {
-      // generate a random character
-      const text = String.fromCharCode(Math.floor(Math.random() * (126 - 33) + 33))
-
       // x coordinate of the column, y coordinate is already given
       const x = ind * 20
       // render the character at (x, y)
-      ctx.fillText(text, x, y)
+      ctx.fillText(getRandomChar(), x, y)
 
       if (y > 100 + Math.random() * 10000) {
         // randomly reset the end of the column if it's at least 100px high
         ypos[ind] = 0
-      }
-      else {
+      } else {
         // otherwise just move the y coordinate for the column 20px down,
         ypos[ind] = y + 20
       }
@@ -60,9 +59,17 @@ export default function TheMatrixAnimation () {
         const cols = Math.floor(canvas.width / 20) + 1
         ypos = Array(cols).fill(0)
 
-        for (let i = 0; i < 300; i++) {
-          matrixAnimationFrame(canvas, ctx, ypos)
-        }
+        ctx.font = '15pt monospace'
+        ypos.forEach((y, i) => {
+          const x = i * 20
+          const startY = Math.floor(Math.random() * (canvas.height - 16 * 20))
+          for (let j = 1; j < 16; j++) {
+            ctx.fillStyle = `rgb(0, ${j * 16}, 0)`
+            ctx.fillText(getRandomChar(), x, startY + j * 20)
+          }
+
+          ypos[i] = startY + 16 * 20
+        })
       }
 
       matrixAnimationFrame(canvas, ctx, ypos)
