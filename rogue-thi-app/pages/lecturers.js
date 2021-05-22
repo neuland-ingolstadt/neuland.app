7b82b144a20e1a6268760db32ae346611f939a1f
Jakob Löw <jakob@m4gnus.de>
2021-05-22 15:37:54 +0200

    /lecturers: improve phone number postprocessing

--- rogue-thi-app/pages/lecturers.js
+++ rogue-thi-app/pages/lecturers.js
@@ -24,11 +24,12 @@ function normalizeLecturers (entries) {
       // try to reformat phone numbers to DIN 5008 International
       tel_dienst: x.tel_dienst
         .trim()
-        .replace(/\(\d+\)/g, '')
-        .replace(/(\d|\/)(\s|-|\/)+(?=\d|\/)/g, '$1')
-        .replace(/^49/g, '+49')
-        .replace(/^\+49841/, '+49 841 ')
-        .replace(/^0841/, '+49 841 '),
+        .replace(/\(0\)/g, '') // remove (0) in +49 (0) 8441
+        .replace(/(\d|\/)(\s|-|\/|\(|\))+(?=\d|\/)/g, '$1') // remove spaces, -, / and braces in numbers
+        .replace(/^-?(\d{4,5})$/, '+49 841 9348$1') // add prefix for suffix-only numbers
+        .replace(/^9348/, '+49 841 9348') // add missing +49 841 prefix to THI numbers
+        .replace(/^49/, '+49') // fix international format
+        .replace(/^((\+?49)|0)\s*841\s*/, '+49 841 '), // fix non-international numbers
       room_short: ((x.raum || '').match(/[A-Z]\s*\d+/g) || [''])[0]
         .replace(/\s+/g, '') || null
     }))
