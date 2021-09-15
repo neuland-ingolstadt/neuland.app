b453f4b1a96bd86d024dd1d91904854e088ec290
Alexander Horn <alexhorn@users.noreply.github.com>
2021-09-15 17:11:01 +0200

    Fix Capacitor HTTP implementation

--- rogue-thi-app/lib/fetch-implementations/capacitor-http.js
+++ rogue-thi-app/lib/fetch-implementations/capacitor-http.js
@@ -4,8 +4,24 @@ import { HttpResponse } from './index'
 
 export default class CapacitorFetchConnection {
   async fetch (url, options) {
+    const ctHeader = options && options.headers && Object.entries(options.headers).find(([key]) => key.toLowerCase() === 'content-type')
+    const mime = ctHeader ? ctHeader[1].split(';')[0] : null
+
+    // parse body because capacitor expects a non-serialized data object
+    let data = undefined
+    if (!options || !options.body) {
+      // mimimi
+    } else if (mime === 'application/x-www-form-urlencoded') {
+      data = Object.fromEntries(new URLSearchParams(options.body).entries())
+    } else if (mime === 'application/json') {
+      data = JSON.parse(options.body)
+    } else {
+      throw new Error(`Unsupported mime type: ${mime}`)
+    }
+
     const response = await Http.request({
       url,
+      data: data,
       ...options
     })
     const body = typeof response.data === 'string' ? response.data : JSON.stringify(response.data)
