8b72b49628aff838fc3aab92b2cdc9f71ef1368b
Jakob Löw <jakob@m4gnus.de>
2022-01-30 21:57:40 +0100

    :bug: :lipstick: fix broken fonts because of the content security policy

--- rogue-thi-app/next.config.js
+++ rogue-thi-app/next.config.js
@@ -68,6 +68,7 @@ module.exports = {
             key: 'Content-Security-Policy',
             value: `default-src 'none';
               img-src 'self' data: https://*.tile.openstreetmap.org;
+              font-src 'self';
               connect-src 'self' wss://proxy.neuland.app;
               style-src 'self' 'unsafe-inline';
               script-src 'self'${isDev ? ' \'unsafe-eval\'' : ''};
