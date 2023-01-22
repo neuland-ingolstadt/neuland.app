c6e4c4adb152e738bdb3f9eb3ae1e774c6d743c7
Fabian Simon Groß <fag8714@thi.de>
2023-01-22 20:49:32 +0100

    First Implementation of a better Filter for the Food Page

--- rogue-thi-app/pages/food.js
+++ rogue-thi-app/pages/food.js
@@ -6,8 +6,8 @@ import ListGroup from 'react-bootstrap/ListGroup'
 import Modal from 'react-bootstrap/Modal'
 import ReactPlaceholder from 'react-placeholder'
 
+import { faExclamationTriangle, faFilter } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
 import AppBody from '../components/page/AppBody'
 import AppContainer from '../components/page/AppContainer'
@@ -36,7 +36,7 @@ export default function Mensa () {
   const [selectedRestaurants, setSelectedRestaurants] = useState(['mensa'])
   const [showMealDetails, setShowMealDetails] = useState(null)
   const [allergenSelection, setAllergenSelection] = useState({})
-  const [showAllergenSelection, setShowAllergenSelection] = useState(false)
+  const [showFilterSelection, setShowFilterSelection] = useState(false)
   const [isStudent, setIsStudent] = useState(true)
 
   useEffect(() => {
@@ -76,7 +76,7 @@ export default function Mensa () {
 
   function saveAllergenSelection () {
     localStorage.selectedAllergens = JSON.stringify(allergenSelection)
-    setShowAllergenSelection(false)
+    setShowFilterSelection(false)
   }
 
   function containsSelectedAllergen (allergens) {
@@ -104,222 +104,244 @@ export default function Mensa () {
   }
 
   return (
-    <AppContainer>
-      <AppNavbar title="Essen" showBack={'desktop-only'}>
-        <AppNavbar.Overflow>
-          <AppNavbar.Overflow.Link variant="link" onClick={() => setShowAllergenSelection(true)}>
-            Allergene auswählen
-          </AppNavbar.Overflow.Link>
-          <AppNavbar.Overflow.Link variant="link" onClick={() => toggleSelectedRestaurant('mensa')}>
-            Mensa {selectedRestaurants.includes('mensa') ? 'ausblenden' : 'einblenden'}
-          </AppNavbar.Overflow.Link>
-          <AppNavbar.Overflow.Link variant="link" onClick={() => toggleSelectedRestaurant('reimanns')}>
-            Reimanns {selectedRestaurants.includes('reimanns') ? 'ausblenden' : 'einblenden'}
-          </AppNavbar.Overflow.Link>
-        </AppNavbar.Overflow>
-      </AppNavbar>
+      <AppContainer>
+        <AppNavbar title="Essen" showBack={'desktop-only'}>
+          <AppNavbar.Button onClick={() => setShowFilterSelection(true)}>
+            <FontAwesomeIcon title="Personalisieren" icon={faFilter} fixedWidth />
+          </AppNavbar.Button>
+        </AppNavbar>
 
-      <AppBody>
-        <ReactPlaceholder type="text" rows={20} ready={foodEntries}>
-          {foodEntries && foodEntries.map((day, idx) =>
-            <ListGroup key={idx}>
-              <h4 className={styles.dateBoundary}>
-                {formatNearDate(day.timestamp)}
-              </h4>
-
-              {day.meals.map((meal, idx) =>
-                <ListGroup.Item
-                  key={idx}
-                  className={styles.item}
-                  onClick={() => setShowMealDetails(meal)}
-                  action
-                >
-                  <div className={styles.left}>
-                    <div className={styles.name}>
-                      {meal.name}
+        <AppBody>
+          <ReactPlaceholder type="text" rows={20} ready={foodEntries}>
+            {foodEntries && foodEntries.map((day, idx) =>
+                <ListGroup key={idx}>
+                  <h4 className={styles.dateBoundary}>
+                    <div className={styles.left}>
+                      {formatNearDate(day.timestamp)}
                     </div>
-                    <div className={styles.room}>
-                      <small style={{ color: containsSelectedAllergen(meal.allergens) && COLOR_WARN }}>
-                        {!meal.allergens && 'Unbekannte Zutaten / Allergene'}
-                        {containsSelectedAllergen(meal.allergens) && (
-                          <span>
+                  </h4>
+
+                  {day.meals.map((meal, idx) =>
+                      <ListGroup.Item
+                          key={idx}
+                          className={styles.item}
+                          onClick={() => setShowMealDetails(meal)}
+                          action
+                      >
+                        <div className={styles.left}>
+                          <div className={styles.name}>
+                            {meal.name}
+                          </div>
+                          <div className={styles.room}>
+                            <small style={{ color: containsSelectedAllergen(meal.allergens) && COLOR_WARN }}>
+                              {!meal.allergens && 'Unbekannte Zutaten / Allergene'}
+                              {containsSelectedAllergen(meal.allergens) && (
+                                  <span>
                             <FontAwesomeIcon title="Warnung" icon={faExclamationTriangle} color={COLOR_WARN} />
-                            {' '}
+                                    {' '}
                           </span>
-                        )}
-                        {meal.flags && meal.flags.map((flag, idx) => (
-                          <span key={idx}>
+                              )}
+                              {meal.flags && meal.flags.map((flag, idx) => (
+                                  <span key={idx}>
                             {idx > 0 && ', '}
-                            <span>
+                                    <span>
                               {flagMap[flag]}
                             </span>
                           </span>
-                        ))}
-                        {meal.allergens && meal.allergens.map((supplement, idx) => (
-                          <span key={idx}>
+                              ))}
+                              {meal.allergens && meal.allergens.map((supplement, idx) => (
+                                  <span key={idx}>
                             {(idx > 0 || meal.flags?.length > 0) && ', '}
-                            <span>
+                                    <span>
                               {supplement}
                             </span>
                           </span>
-                        ))}
-                      </small>
-                    </div>
-                  </div>
-                  <div className={styles.right}>
-                    {getUserSpecificPrice(meal)}
-                    <br />
-                    {meal.restaurant}
-                  </div>
-                </ListGroup.Item>
-              )}
-            </ListGroup>
-          )}
-          {foodEntries && foodEntries.length === 0 &&
-            <ListGroup>
-              <ListGroup.Item>
-                Der Speiseplan ist leer.
-              </ListGroup.Item>
-            </ListGroup>
-          }
-        </ReactPlaceholder>
-
-        <br />
-
-        <Modal show={showMealDetails} onHide={() => setShowMealDetails(null)}>
-          <Modal.Header closeButton>
-            <Modal.Title>Erläuterung</Modal.Title>
-          </Modal.Header>
-
-          <Modal.Body>
-            <h5>Anmerkungen</h5>
-            {showMealDetails?.flags === null && 'Unbekannt.'}
-            {showMealDetails?.flags?.length === 0 && 'Keine.'}
-            <ul>
-              {showMealDetails?.flags?.map(flag => (
-                <li key={flag}>
-                  <strong>{flag}</strong>
-                  {' – '}
-                  {flagMap[flag] || FALLBACK_ALLERGEN}
-                </li>
-              ))}
-            </ul>
-
-            <h5>Allergene</h5>
-            {showMealDetails?.allergens === null && 'Unbekannt.'}
-            {showMealDetails?.allergens?.length === 0 && 'Keine.'}
-            <ul>
-              {showMealDetails?.allergens?.map(key => (
-                <li key={key} style={{ color: containsSelectedAllergen([key]) && COLOR_WARN }}>
-                  {containsSelectedAllergen([key]) && (
-                    <span>
-                      <FontAwesomeIcon icon={faExclamationTriangle} color={COLOR_WARN} />
-                      {' '}
-                    </span>
+                              ))}
+                            </small>
+                          </div>
+                        </div>
+                        <div className={styles.right}>
+                          {getUserSpecificPrice(meal)}
+                          <br />
+                          {meal.restaurant}
+                        </div>
+                      </ListGroup.Item>
                   )}
-                  {' '}
-                  <strong>{key}</strong>
-                  {' – '}
-                  {allergenMap[key] || FALLBACK_ALLERGEN}
-                </li>
-              ))}
-            </ul>
+                </ListGroup>
+            )}
+            {foodEntries && foodEntries.length === 0 &&
+                <ListGroup>
+                  <ListGroup.Item>
+                    Der Speiseplan ist leer.
+                  </ListGroup.Item>
+                </ListGroup>
+            }
+          </ReactPlaceholder>
 
-            <h5>Nährwerte</h5>
+          <br />
 
-            {(showMealDetails?.nutrition && (
+          <Modal show={showMealDetails} onHide={() => setShowMealDetails(null)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Erläuterung</Modal.Title>
+            </Modal.Header>
 
+            <Modal.Body>
+              <h5>Anmerkungen</h5>
+              {showMealDetails?.flags === null && 'Unbekannt.'}
+              {showMealDetails?.flags?.length === 0 && 'Keine.'}
+              <ul>
+                {showMealDetails?.flags?.map(flag => (
+                    <li key={flag}>
+                      <strong>{flag}</strong>
+                      {' – '}
+                      {flagMap[flag] || FALLBACK_ALLERGEN}
+                    </li>
+                ))}
+              </ul>
+
+              <h5>Allergene</h5>
+              {showMealDetails?.allergens === null && 'Unbekannt.'}
+              {showMealDetails?.allergens?.length === 0 && 'Keine.'}
+              <ul>
+                {showMealDetails?.allergens?.map(key => (
+                    <li key={key} style={{ color: containsSelectedAllergen([key]) && COLOR_WARN }}>
+                      {containsSelectedAllergen([key]) && (
+                          <span>
+                      <FontAwesomeIcon icon={faExclamationTriangle} color={COLOR_WARN} />
+                            {' '}
+                    </span>
+                      )}
+                      {' '}
+                      <strong>{key}</strong>
+                      {' – '}
+                      {allergenMap[key] || FALLBACK_ALLERGEN}
+                    </li>
+                ))}
+              </ul>
+
+              <h5>Nährwerte</h5>
+
+              {(showMealDetails?.nutrition && (
+
+                  <ul>
+                    <li>
+                      <strong>Energie</strong>:{' '}
+                      {showMealDetails?.nutrition.kj ? showMealDetails?.nutrition.kj + ' kJ' : ''} / &nbsp;
+                      {showMealDetails?.nutrition.kcal ? showMealDetails?.nutrition.kcal + ' kcal' : ''}
+                    </li>
+                    <li>
+                      <strong>Fett</strong>:{' '}
+                      {formatGram(showMealDetails?.nutrition.fat)}
+                      <br /><strong>davon gesättigte Fettsäuren</strong>: {formatGram(showMealDetails?.nutrition.fatSaturated)}
+                    </li>
+                    <li>
+                      <strong>Kohlenhydrate</strong>:{' '}
+                      {formatGram(showMealDetails?.nutrition.carbs)}
+                      <br /><strong>davon Zucker</strong>: {formatGram(showMealDetails?.nutrition.sugar)}
+                    </li>
+                    <li>
+                      <strong>Ballaststoffe</strong>:{' '}
+                      {formatGram(showMealDetails?.nutrition.fiber)}
+                    </li>
+                    <li>
+                      <strong>Eiweiß</strong>:{' '}
+                      {formatGram(showMealDetails?.nutrition.protein)}
+                    </li>
+                    <li>
+                      <strong>Salz</strong>:{' '}
+                      {formatGram(showMealDetails?.nutrition.salt)}
+                    </li>
+                  </ul>)) || (
+                  <p>Unbekannt.</p>
+              )}
+
+              <h5>Preise</h5>
               <ul>
                 <li>
-                  <strong>Energie</strong>:{' '}
-                  {showMealDetails?.nutrition.kj ? showMealDetails?.nutrition.kj + ' kJ' : ''} / &nbsp;
-                  {showMealDetails?.nutrition.kcal ? showMealDetails?.nutrition.kcal + ' kcal' : ''}
+                  <strong>Studierende</strong>:{' '}
+                  {formatPrice(showMealDetails?.prices.student)}
                 </li>
                 <li>
-                  <strong>Fett</strong>:{' '}
-                  {formatGram(showMealDetails?.nutrition.fat)}
-                  <br /><strong>davon gesättigte Fettsäuren</strong>: {formatGram(showMealDetails?.nutrition.fatSaturated)}
+                  <strong>Mitarbeitende</strong>:{' '}
+                  {formatPrice(showMealDetails?.prices.employee)}
                 </li>
                 <li>
-                  <strong>Kohlenhydrate</strong>:{' '}
-                  {formatGram(showMealDetails?.nutrition.carbs)}
-                  <br /><strong>davon Zucker</strong>: {formatGram(showMealDetails?.nutrition.sugar)}
+                  <strong>Gäste</strong>:{' '}
+                  {formatPrice(showMealDetails?.prices.guest)}
                 </li>
-                <li>
-                  <strong>Ballaststoffe</strong>:{' '}
-                  {formatGram(showMealDetails?.nutrition.fiber)}
-                </li>
-                <li>
-                  <strong>Eiweiß</strong>:{' '}
-                  {formatGram(showMealDetails?.nutrition.protein)}
-                </li>
-                <li>
-                  <strong>Salz</strong>:{' '}
-                  {formatGram(showMealDetails?.nutrition.salt)}
-                </li>
-              </ul>)) || (
-                <p>Unbekannt.</p>
-            )}
+              </ul>
 
-            <h5>Preise</h5>
-            <ul>
-              <li>
-                <strong>Studierende</strong>:{' '}
-                {formatPrice(showMealDetails?.prices.student)}
-              </li>
-              <li>
-                <strong>Mitarbeitende</strong>:{' '}
-                {formatPrice(showMealDetails?.prices.employee)}
-              </li>
-              <li>
-                <strong>Gäste</strong>:{' '}
-                {formatPrice(showMealDetails?.prices.guest)}
-              </li>
-            </ul>
+              <p>
+                <strong>Angaben ohne Gewähr. </strong>
+                <br />
+                Bitte prüfe die Angaben auf den Infobildschirmen, bevor du etwas konsumiert.
+                Die Nährwertangaben beziehen sich auf eine durchschnittliche Portion.
+              </p>
+            </Modal.Body>
 
-            <p>
-              <strong>Angaben ohne Gewähr. </strong>
-              <br />
-              Bitte prüfe die Angaben auf den Infobildschirmen, bevor du etwas konsumiert.
-              Die Nährwertangaben beziehen sich auf eine durchschnittliche Portion.
-            </p>
-          </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={() => setShowMealDetails(null)}>OK</Button>
+            </Modal.Footer>
+          </Modal>
 
-          <Modal.Footer>
-            <Button variant="primary" onClick={() => setShowMealDetails(null)}>OK</Button>
-          </Modal.Footer>
-        </Modal>
+          <Modal show={showFilterSelection} onHide={() => setShowFilterSelection(false)}>
+            <Modal.Header closeButton>
+              <Modal.Title>Filter</Modal.Title>
+            </Modal.Header>
 
-        <Modal show={showAllergenSelection} onHide={() => setShowAllergenSelection(false)}>
-          <Modal.Header closeButton>
-            <Modal.Title>Allergene auswählen</Modal.Title>
-          </Modal.Header>
+            <Modal.Body>
+              <Modal.Header>
+                Restaurant
+              </Modal.Header>
 
-          <Modal.Body>
-            <p>
-              Wähle die Allergene aus, bei denen du gesondert gewarnt werden möchtest. Deine Angaben werden nur lokal auf deinem Gerät gespeichert und nicht übermittelt. (Auch nicht an die THI.)
-            </p>
+              <Form>
+                <Modal.Body>
+                  <Form.Check
+                      id={'restaurant-checkbox-mensa'}
+                      label={<span><strong>Mensa</strong></span>}
+                      checked={selectedRestaurants.includes('mensa')}
+                      onChange={() => toggleSelectedRestaurant('mensa')}
+                  />
+                  <Form.Check
+                      id={'restaurant-checkbox-reimanns'}
+                      label={<span><strong>Reimanns</strong></span>}
+                      checked={selectedRestaurants.includes('reimanns')}
+                      onChange={() => toggleSelectedRestaurant('reimanns')}
+                  />
+                </Modal.Body>
+              </Form>
 
-            <Form>
-              {Object.entries(allergenMap).map(([key, value]) => (
-                <Form.Check
-                  key={key}
-                  id={'allergen-checkbox-' + key}
-                  label={<span><strong>{key}</strong>{' – '}{value}</span>}
-                  checked={allergenSelection[key] || false}
-                  onChange={e => setAllergenSelection({ ...allergenSelection, [key]: e.target.checked })}
-                />
-              ))}
-            </Form>
-          </Modal.Body>
+              <Modal.Header>
+                  Allergene
+              </Modal.Header>
 
-          <Modal.Footer>
-            <Button variant="primary" onClick={() => saveAllergenSelection()}>OK</Button>
-          </Modal.Footer>
-        </Modal>
-      </AppBody>
+              <Modal.Body>
+                <Form>
+                  {Object.entries(allergenMap).map(([key, value]) => (
+                      <Form.Check
+                          key={key}
+                          id={'allergen-checkbox-' + key}
+                          label={<span><strong>{key}</strong>{' – '}{value}</span>}
+                          checked={allergenSelection[key] || false}
+                          onChange={e => setAllergenSelection({ ...allergenSelection, [key]: e.target.checked })}
+                      />
+                  ))}
+                </Form>
+              </Modal.Body>
 
-      <AppTabbar />
-    </AppContainer>
+              {/** Modal.Header: Essenspezifisch */}
+              <p>
+                Wähle deine Präferenzen aus. Deine Angaben werden nur lokal auf deinem Gerät gespeichert und nicht übermittelt. (Auch nicht an die THI.)
+              </p>
+            </Modal.Body>
+
+            <Modal.Footer>
+              <Button variant="primary" onClick={() => saveAllergenSelection()}>OK</Button>
+            </Modal.Footer>
+          </Modal>
+        </AppBody>
+
+        <AppTabbar />
+      </AppContainer>
   )
 }
