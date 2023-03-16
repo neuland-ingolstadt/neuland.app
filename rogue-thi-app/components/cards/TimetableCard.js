52ee37f30ee0ab1aa561b7a0ecfd622059ddfff9
Robert Eggl <github@roberteggl.dev>
2023-03-16 14:29:11 +0100

    improves timetable card

--- rogue-thi-app/components/cards/TimetableCard.js
+++ rogue-thi-app/components/cards/TimetableCard.js
@@ -9,19 +9,18 @@ import { formatFriendlyTime, formatNearDate } from '../../lib/date-utils'
 import { getFriendlyTimetable, getTimetableEntryName } from '../../lib/backend-utils/timetable-utils'
 import BaseCard from './BaseCard'
 import { NoSessionError } from '../../lib/backend/thi-session-handler'
-import { useTime } from '../../lib/hooks/time-hook'
 
 /**
  * Dashboard card for the timetable.
  */
 export default function TimetableCard () {
-  const time = useTime()
   const router = useRouter()
   const [timetable, setTimetable] = useState(null)
   const [timetableError, setTimetableError] = useState(null)
+  const [currentTime, setCurrentTime] = useState(new Date())
 
   useEffect(() => {
-    async function load () {
+    async function loadTimetable () {
       try {
         setTimetable(await getFriendlyTimetable(new Date(), false))
       } catch (e) {
@@ -33,8 +32,13 @@ export default function TimetableCard () {
         }
       }
     }
-    load()
-  }, [router, time])
+    loadTimetable()
+  }, [router])
+
+  useEffect(() => {
+    const interval = setInterval(() => setCurrentTime(new Date()), 30000)
+    return () => clearInterval(interval)
+  }, [])
 
   return (
     <BaseCard
@@ -44,24 +48,32 @@ export default function TimetableCard () {
     >
       <ReactPlaceholder type="text" rows={5} ready={timetable || timetableError}>
         <ListGroup variant="flush">
-          {(timetable && timetable.slice(0, 2).map((x, i) => (
-            <ListGroup.Item key={i}>
-              <div>
-                {getTimetableEntryName(x).shortName} in {x.raum}
-              </div>
-              {((x.startDate < new Date() && x.endDate > new Date()) &&
-                (x.endDate - new Date()) <= 30 * 60 * 1000 &&
-                <div className="text-muted">Endet in {Math.round((x.endDate - new Date()) / 1000 / 60)} min</div>) ||
-                ((x.startDate < new Date() && x.endDate > new Date()) &&
-                  (x.endDate - new Date()) > 30 * 60 * 1000 &&
-                  <div className="text-muted">Endet um {formatFriendlyTime(x.endDate)}</div>) ||
-                ((x.startDate > new Date() && new Date(x.startDate) <= new Date(new Date().getTime() + 30 * 60 * 1000)) &&
-                  <div className="text-muted">Beginnt in {Math.round((x.startDate - new Date()) / 1000 / 60)} min</div>) ||
-                ((x.startDate > new Date() && new Date(x.startDate) > new Date(new Date().getTime() + 30 * 60 * 1000)) &&
-                  <div className="text-muted">{formatNearDate(x.startDate)} um {formatFriendlyTime(x.startDate)}</div>) ||
-                null}
-            </ListGroup.Item>
-          ))) ||
+          {(timetable && timetable.slice(0, 2).map((x, i) => {
+            const isSoon = (x.startDate > currentTime && new Date(x.startDate) <= new Date(currentTime.getTime() + 30 * 60 * 1000))
+            const isOngoing = x.startDate < currentTime && x.endDate > currentTime
+            const isEndingSoon = isOngoing && (x.endDate - currentTime) <= 30 * 60 * 1000
+            const isNotSoonOrOngoing = !isSoon && !isOngoing
+
+            let text = null
+            if (isEndingSoon) {
+              text = <div className="text-muted">Endet in {Math.ceil((x.endDate - currentTime) / 1000 / 60)} min</div>
+            } else if (isOngoing) {
+              text = <div className="text-muted">Endet um {formatFriendlyTime(x.endDate)}</div>
+            } else if (isSoon) {
+              text = <div className="text-muted">Beginnt in {Math.ceil((x.startDate - currentTime) / 1000 / 60)} min</div>
+            } else if (isNotSoonOrOngoing) {
+              text = <div className="text-muted">{formatNearDate(x.startDate)} um {formatFriendlyTime(x.startDate)}</div>
+            }
+
+            return (
+              <ListGroup.Item key={i}>
+                <div>
+                  {getTimetableEntryName(x).shortName} in {x.raum}
+                </div>
+                {text}
+              </ListGroup.Item>
+            )
+          })) ||
             (timetable && timetable.length === 0 &&
               <ListGroup.Item>Du hast heute keine Vorlesungen mehr.</ListGroup.Item>) ||
             (timetableError &&
