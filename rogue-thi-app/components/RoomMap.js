e380a65b8d2e0b6a2b65ee6f8c54f3dae04d9b2b
Philipp Opheys <philipp@opheys.dev>
2023-03-30 12:50:20 +0200

    hide room suggestions button for non students

--- rogue-thi-app/components/RoomMap.js
+++ rogue-thi-app/components/RoomMap.js
@@ -13,6 +13,7 @@ import { filterRooms, getNextValidDate } from '../lib/backend-utils/rooms-utils'
 import { formatFriendlyTime, formatISODate, formatISOTime } from '../lib/date-utils'
 import { useLocation } from '../lib/hooks/geolocation'
 
+import { USER_STUDENT, useUserKind } from '../lib/hooks/user-kind'
 import styles from '../styles/RoomMap.module.css'
 
 const SPECIAL_ROOMS = {
@@ -50,6 +51,7 @@ export default function RoomMap ({ highlight, roomData }) {
   const router = useRouter()
   const searchField = useRef()
   const location = useLocation()
+  const userKind = useUserKind()
   const [searchText, setSearchText] = useState(highlight ? highlight.toUpperCase() : '')
   const [availableRooms, setAvailableRooms] = useState(null)
 
@@ -125,7 +127,7 @@ export default function RoomMap ({ highlight, roomData }) {
       }
     }
     load()
-  }, [router, highlight])
+  }, [router, highlight, userKind])
 
   /**
    * Removes focus from the search.
@@ -226,12 +228,16 @@ export default function RoomMap ({ highlight, roomData }) {
           </a>
         </Link>
 
-        <br />
-        <Link href="/rooms/suggestions">
-          <a className={styles.linkToSearch}>
-            Raum Vorschläge
-          </a>
-        </Link>
+        {userKind === USER_STUDENT &&
+          <div>
+            <br />
+            <Link href="/rooms/suggestions">
+              <a className={styles.linkToSearch}>
+                Raum Vorschläge
+              </a>
+            </Link>
+          </div>
+        }
       </Form>
 
       <MapContainer
