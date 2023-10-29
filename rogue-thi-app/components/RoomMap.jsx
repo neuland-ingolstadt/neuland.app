d13f16ad43ac5b561f44fc6b02468a958d4c6434
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2023-10-29 21:23:18 +0100

    undo latest change

--- rogue-thi-app/components/RoomMap.jsx
+++ rogue-thi-app/components/RoomMap.jsx
@@ -140,7 +140,7 @@ export default function RoomMap ({ highlight, roomData }) {
     const filteredCenter = count > 0 ? [lon / count, lat / count] : DEFAULT_CENTER
 
     return [filtered, filteredCenter]
-  }, [searchText, allRooms, loadRoomAvailability])
+  }, [searchText, allRooms])
 
   useEffect(() => {
     async function load () {
