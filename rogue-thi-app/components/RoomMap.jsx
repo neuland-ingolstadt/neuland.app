3ed7ea3fcda089ef6729a5c5572da273c67baa2d
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2023-11-22 14:38:40 +0100

    bugfix: roomAvailability shows wrong time DEBUG

--- rogue-thi-app/components/RoomMap.jsx
+++ rogue-thi-app/components/RoomMap.jsx
@@ -118,6 +118,7 @@ export default function RoomMap ({ highlight, roomData }) {
         )
       return [room, availability]
     }))
+    // console.log(roomAvailabilityList['G308'])
 
     setRoomAvailabilityList(roomAvailabilityList)
   }
