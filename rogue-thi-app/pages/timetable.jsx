d148af3d0f6875974ab551f096716615c7a0fd78
Kevin Ploß <65730791+pl0ss@users.noreply.github.com>
2023-12-15 13:23:46 +0100

    console.log removed

--- rogue-thi-app/pages/timetable.jsx
+++ rogue-thi-app/pages/timetable.jsx
@@ -192,7 +192,6 @@ export default function Timetable () {
       return [room, availability]
     }))
 
-    console.log(roomAvailabilityList)
     setRoomAvailabilityList(roomAvailabilityList)
   }
 
