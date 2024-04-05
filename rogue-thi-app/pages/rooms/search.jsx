a920178cedcbea34dca42de1c952c10726fcaf11
Martin Lauff <72349751+MartinLauff@users.noreply.github.com>
2024-04-05 19:23:57 +0200

    🐛 fixes #339

--- rogue-thi-app/pages/rooms/search.jsx
+++ rogue-thi-app/pages/rooms/search.jsx
@@ -147,7 +147,10 @@ export default function RoomSearch() {
                 value={building}
                 onChange={(e) => setBuilding(e.target.value)}
               >
-                <option key={BUILDINGS_ALL}>
+                <option
+                  key={BUILDINGS_ALL}
+                  value={BUILDINGS_ALL}
+                >
                   {t('rooms.search.buildingsAll')}
                 </option>
                 {BUILDINGS.map((b) => (
