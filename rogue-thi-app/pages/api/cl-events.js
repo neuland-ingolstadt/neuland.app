aedb9eda9965862a5ebdd379f83da864d8a2c30c
Philipp Opheys <philipp@opheys.dev>
2024-10-14 11:45:05 +0200

    ✨ add location to iCal

--- rogue-thi-app/pages/api/cl-events.js
+++ rogue-thi-app/pages/api/cl-events.js
@@ -49,6 +49,7 @@ export default async function handler(req, res) {
         id: event.id,
         summary: event.title,
         description: `Veranstalter: ${event.organizer}`,
+        location: event.location,
         start,
         end,
       })
